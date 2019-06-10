6447d10492a559d0f32740722dffbaca0ef05610
Eduardo de S. S. Thais <stag012@benner.com.br>
2019-06-10 10:50:20 -0300

    ASP.NET 50%

--- ASP.NET/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
+++ ASP.NET/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -1,24 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿
 using CaelumEstoque.DAO;
 using CaelumEstoque.Models;
-
+using System.Collections.Generic;
+using System.Web.Mvc;
 namespace CaelumEstoque.Controllers
 {
     public class ProdutoController : Controller
     {
-        // GET: Produto
+        //
+        // GET: /Produto/
+
         public ActionResult Index()
         {
             ProdutosDAO dao = new ProdutosDAO();
             IList<Produto> produtos = dao.Lista();
             ViewBag.Produtos = produtos;
-
-
             return View();
+        }
+
+        public ActionResult Form()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            ViewBag.Produto = new Produto();
+            ViewBag.Categorias = dao.Lista();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            int idDaInformatica = 1;
+            if (produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.InformaticaComPrecoInvalido", "Produtos da categoria informática devem ter preço maior do que 100");
+            }
+            if (produto.Categoria.Id.Equals(informaticaId) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.InformaticaComPrecoInvalido", "Produtos da categoria informática devem ter preço maior do que 100");
+            }
+            if (ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Produto = produto;
+                CategoriasDAO categoriasDAO = new CategoriasDAO();
+                ViewBag.Categorias = categoriasDAO.Lista();
+                return View("Form");
+            }
         }
     }
 }
