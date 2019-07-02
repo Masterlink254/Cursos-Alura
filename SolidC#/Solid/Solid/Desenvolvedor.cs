070f6b40e96677fb8693dd0d6f84053093c24c0d
Eduardo de S. S. Thais <stag012@benner.com.br>
2019-07-02 11:42:51 -0300

    SOLID

---
+++ SolidC#/Solid/Solid/Desenvolvedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid
+{
+    class Desenvolvedor : Cargo
+    {
+        public Desenvolvedor(IRegraDeCalculo regra) : base(regra)
+        {
+          
+        }
+    }
+}
---
+++ SolidC#/Solid/Solid/Tester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid
+{
+    class Tester : Cargo
+    {
+        public Tester(IRegraDeCalculo regra) : base(regra)
+        {
+            
+        }
+    }
+}
